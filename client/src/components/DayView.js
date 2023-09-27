dc88604adfb22303138de430a1d9b3e91e1bf504
Curtisodell1 <46852021+Curtisodell1@users.noreply.github.com>
2023-09-27 11:13:55 -0600

    various changes throughout form and dashbord display

--- client/src/components/DayView.js
+++ client/src/components/DayView.js
@@ -1,42 +1,71 @@
 
 
 function DayView(){
-    return (
-        <div className="dayPage"> whole page
-            <div className="inputContainer"> container for feelings
-                <h1 className="feelingHeadLine">How are you feeling?</h1>
-                <form className="feelingForm">
-                    <div>
-                        <span>
-                            <label className="activityLabel">Did you go for a walk?</label>
-                            <label> Yes!</label>
-                            <input className="radioButton" type="radio"></input>
-                            <label> Not yet...</label>
-                            <input className="radioButton" type="radio"></input>
-                        </span>
-                    </div>
-                    <div>
-                        <span>
-                            <label className="activityLabel">Did you work on a project?</label>
-                            <label> Yes!</label>
-                            <input className="radioButton" type="radio"></input>
-                            <label> Not yet...</label>
-                            <input className="radioButton" type="radio"></input>
-                        </span>
-                    </div>
-                    <div>
-                        <span>
-                            <label className="activityLabel"> Did you do your other thing?</label>
-                            <label> Yes!</label>
-                            <input className="radioButton" type="radio"></input>
-                            <label> Not yet...</label>
-                            <input className="radioButton" type="radio"></input>
-                        </span>
-                    </div>
-                </form>
-            </div>
-        </div>
 
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log()
+    }
+
+
+    return (
+        <div className="dayPage">
+            <div className="inputContainer">
+                <form onSubmit={handleSubmit} className="feelingForm">
+                    <h1 className="feelingHeadLine">How are you feeling?</h1>
+                    <div class="slidecontainer">
+                    <input type="range" min="1" max="10" value="5" class="slider" id="myRange"/>
+                    </div>
+                        <div>
+                                <label className="activityLabel">Did you go for a walk?</label>
+                            <div className="entryDivs">
+                                <span className="yes">
+                                <label for="work" className="choiceLabels"> Yes!</label>
+                                <input className="radioButton" type="radio" name="walk"></input>
+                                </span>
+                                <span className="yes">
+                                <label className="choiceLabels"> Not yet...</label>
+                                <input className="radioButton" type="radio" name="walk"></input>
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                                <label className="activityLabel">Did you work on a project?</label>
+                            <div className="entryDivs">
+                                <span className="yes">
+                                <label className="choiceLabels"> Yes!</label>
+                                <input className="radioButton" type="radio" name="work"></input>
+                                </span>
+                                <span className="yes">
+                                <label className="choiceLabels"> Not yet...</label>
+                                <input className="radioButton" type="radio" name="work"></input>
+                                </span>
+                            </div>
+                        </div>
+                        <div>
+                                <label className="activityLabel"> Did you do your other thing?</label>
+                            <span className="entryDivs">
+                                <span className="yes">  
+                                <label className="choiceLabels"> Yes!</label>
+                                <input className="radioButton" type="radio" name="other"></input>
+                                </span>
+                                <span className="yes">
+                                <label className="choiceLabels"> Not yet...</label>
+                                <input className="radioButton" type="radio" name="other"></input>
+                                </span>
+                            </span>
+                        </div>
+                            <div className="entryDivs">
+                                <label className="activityLabel">What felt good so far today?: </label>
+                                <div className="entryDivs">
+                                <input id="inputField" type="text"/>
+                                </div>
+                        </div>
+                            <input 
+                            type="submit" />
+                </form>
+        </div>
+        </div>
     )
 }
 
